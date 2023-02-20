88c74dc74989ad33202e4603cd139280f2d2f037
Charlie Gottlieb <90010456+Gottlieb613@users.noreply.github.com>
2023-02-19 22:21:05 -0500

    Completed lc4_regfile.v, no errors all tests pas

--- lab3-regfile/lc4_regfile.v
+++ lab3-regfile/lc4_regfile.v
@@ -1,5 +1,7 @@
 /* TODO: Names of all group members
+   Charlie Gottlieb, Keith Bonwitt
  * TODO: PennKeys of all group members
+   cbg613, kbonwitt
  *
  * lc4_regfile.v
  * Implements an 8-register register file parameterized on word size.
@@ -40,57 +42,55 @@ module lc4_regfile #(parameter n = 16)
    wire [n-1:0] o_r7;
    
    Nbit_reg #(.n(16)) r0
-   (.in(i_wdata), .out(o_r0)
-    .clk(clk), .we(i_rd == 3'b0), .gwe(gwe), .rst(rst));
+   (.in(i_wdata), .out(o_r0),
+    .clk(clk), .we(i_rd == 3'b0 && i_rd_we), .gwe(gwe), .rst(rst));
    
     Nbit_reg #(.n(16)) r1
-    (.in(i_wdata), .out(o_r1)
-     .clk(clk), .we(i_rd == 3'b1), .gwe(gwe), .rst(rst));
+    (.in(i_wdata), .out(o_r1),
+     .clk(clk), .we(i_rd == 3'b1 && i_rd_we), .gwe(gwe), .rst(rst));
    
     Nbit_reg #(.n(16)) r2
-    (.in(i_wdata), .out(o_r2)
-     .clk(clk), .we(i_rd == 3'b10), .gwe(gwe), .rst(rst));
+    (.in(i_wdata), .out(o_r2),
+     .clk(clk), .we(i_rd == 3'b10 && i_rd_we), .gwe(gwe), .rst(rst));
     
     Nbit_reg #(.n(16)) r3
-    (.in(i_wdata), .out(o_r3)
-     .clk(clk), .we(i_rd == 3'b11), .gwe(gwe), .rst(rst));
+    (.in(i_wdata), .out(o_r3),
+     .clk(clk), .we(i_rd == 3'b11 && i_rd_we), .gwe(gwe), .rst(rst));
     
     Nbit_reg #(.n(16)) r4
-    (.in(i_wdata), .out(o_r4)
-     .clk(clk), .we(i_rd == 3'b100), .gwe(gwe), .rst(rst));
+    (.in(i_wdata), .out(o_r4),
+     .clk(clk), .we(i_rd == 3'b100 && i_rd_we), .gwe(gwe), .rst(rst));
     
     Nbit_reg #(.n(16)) r5
-    (.in(i_wdata), .out(o_r5)
-     .clk(clk), .we(i_rd == 3'b101), .gwe(gwe), .rst(rst));
+    (.in(i_wdata), .out(o_r5),
+     .clk(clk), .we(i_rd == 3'b101 && i_rd_we), .gwe(gwe), .rst(rst));
     
     Nbit_reg #(.n(16)) r6
-    (.in(i_wdata), .out(o_r6)
-     .clk(clk), .we(i_rd == 3'b110), .gwe(gwe), .rst(rst));
+    (.in(i_wdata), .out(o_r6),
+     .clk(clk), .we(i_rd == 3'b110 && i_rd_we), .gwe(gwe), .rst(rst));
     
     Nbit_reg #(.n(16)) r7
-    (.in(i_wdata), .out(o_r7)
-     .clk(clk), .we(i_rd == 3'b111), .gwe(gwe), .rst(rst));
+    (.in(i_wdata), .out(o_r7),
+     .clk(clk), .we(i_rd == 3'b111 && i_rd_we), .gwe(gwe), .rst(rst));
    
      
-     assign o_rsdata = (i_rs == 3'b0) ? o_r0 :
+     assign o_rs_data = (i_rs == 3'b0) ? o_r0 :
                        (i_rs == 3'b1) ? o_r1 :
                        (i_rs == 3'b10) ? o_r2 :
                        (i_rs == 3'b11) ? o_r3 :
                        (i_rs == 3'b100) ? o_r4 :
                        (i_rs == 3'b101) ? o_r5 :
                        (i_rs == 3'b110) ? o_r6 :
-                       (i_rs == 3'b111) ? o_r7 :
-                       n'b0;
+                       o_r7;
     
-     assign o_rtdata = (i_rt == 3'b0) ? o_r0 :
+     assign o_rt_data = (i_rt == 3'b0) ? o_r0 :
                        (i_rt == 3'b1) ? o_r1 :
                        (i_rt == 3'b10) ? o_r2 :
                        (i_rt == 3'b11) ? o_r3 :
                        (i_rt == 3'b100) ? o_r4 :
                        (i_rt == 3'b101) ? o_r5 :
                        (i_rt == 3'b110) ? o_r6 :
-                       (i_rt == 3'b111) ? o_r7 :
-                       n'b0;
+                       o_r7;
      
      
    
