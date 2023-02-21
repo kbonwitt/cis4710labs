2272f0e7588ac8fafc8d781324e4a3712d3998ea
Gottlieb613 <charliegottlieb@gmail.com>
2023-02-21 11:15:50 -0500

    Copied alu, cla, divider from prev. labs to lab3

--- lab3-singlecycle/lc4_alu.v
+++ lab3-singlecycle/lc4_alu.v
@@ -1,4 +1,5 @@
-/* TODO: name and PennKeys of all group members here */
+/* INSERT NAME AND PENNKEY HERE */
+//Names: Charlie Gottlieb cbg613, Keith Bonwitt kbonwitt
 
 `timescale 1ns / 1ps
 `default_nettype none
@@ -8,6 +9,192 @@ module lc4_alu(input  wire [15:0] i_insn,
                input wire [15:0]  i_r1data,
                input wire [15:0]  i_r2data,
                output wire [15:0] o_result);
+      
+      wire [3:0] opcode;
+      wire [2:0] midbits;
+      assign opcode = i_insn[15:12];
+      assign midbits = i_insn[5:3]; //these 'midbits' are helpful to distinguish between ADD/SUB/etc and AND/OR/etc
+
+      wire BRANCH; 
+      assign BRANCH = (opcode == 4'b0); 
+      
+      wire ADD, SUB, MUL, DIV, ADDIMM;
+      assign ADD = (opcode == 4'b1 && midbits == 3'b0);
+      assign MUL = (opcode == 4'b1 && midbits == 3'b1);
+      assign SUB = (opcode == 4'b1 && midbits == 3'b10);
+      assign DIV = (opcode == 4'b1 && midbits == 3'b11);
+      assign ADDIMM = (opcode == 4'b1 && i_insn[5] == 1'b1); //for ADDIMM, just the most significant midbit must be 1
+
+      wire CMP, CMPU, CMPI, CMPIU;
+      assign CMP = (opcode == 4'b10 && i_insn[8:7] == 2'b0);
+      assign CMPU = (opcode == 4'b10 && i_insn[8:7] == 2'b1);
+      assign CMPI = (opcode == 4'b10 && i_insn[8:7] == 2'b10);
+      assign CMPIU = (opcode == 4'b10 && i_insn[8:7] == 2'b11);
+
+      wire JSRR, JSR;
+      assign JSRR = (i_insn[15:11] == 5'b01000);
+      assign JSR = (i_insn[15:11] == 5'b01001);
+
+      wire AND, NOT, OR, XOR, ANDIMM;
+      assign AND = (opcode == 4'b101 && midbits == 3'b0);
+      assign NOT = (opcode == 4'b101 && midbits == 3'b1);
+      assign OR = (opcode == 4'b101 && midbits == 3'b10);
+      assign XOR = (opcode == 4'b101 && midbits == 3'b11);
+      assign ANDIMM = (opcode == 4'b101 && midbits[2] == 1'b1); //for ANDIMM, just the most significant midbit must be 1
+
+      wire LDR, STR;
+      assign LDR = (opcode == 4'b0110);
+      assign STR = (opcode == 4'b0111);
+
+      wire RTI, CONST, HICONST;
+      assign RTI = (opcode == 4'b1000);
+      assign CONST = (opcode == 4'b1001);
+      assign HICONST = (opcode == 4'b1101);
+
+      wire SLL, SRA, SRL, MOD;
+      assign SLL = (opcode == 4'b1010 && midbits[2:1] == 2'b0);
+      assign SRA = (opcode == 4'b1010 && midbits[2:1] == 2'b1);
+      assign SRL = (opcode == 4'b1010 && midbits[2:1] == 2'b10);
+      assign MOD = (opcode == 4'b1010 && midbits[2:1] == 2'b11);
+
+      wire JMPR, JMP;
+      assign JMPR = (i_insn[15:11] == 5'b11000);
+      assign JMP = (i_insn[15:11] == 5'b11001);
+
+      wire TRAP;
+      assign TRAP = (opcode == 4'b1111);
+
+            //Is it necessary to state that these are signed?
+      wire signed [4:0] IMM5;
+      wire signed [5:0] IMM6;
+      wire signed [6:0] IMM7;
+      wire signed [7:0] IMM8; 
+      wire signed [8:0] IMM9; 
+      wire signed [10:0] IMM11;
+      assign IMM5 = i_insn[4:0];
+      assign IMM6 = i_insn[5:0];
+      assign IMM7 = i_insn[6:0];
+      assign IMM8 = i_insn[7:0];
+      assign IMM9 = i_insn[8:0];
+      assign IMM11 = i_insn[10:0];
+
+            //is there a way to indicate that these are UNsigned?
+      wire [3:0] UIMM4;
+      wire [6:0] UIMM7;
+      wire [7:0] UIMM8;
+      assign UIMM4 = i_insn[3:0];
+      assign UIMM7 = i_insn[6:0];
+      assign UIMM8 = i_insn[7:0];
+
+      /*** END DECODER ***/
+
+
+      //insns that use the CLA: add, sub, addimm, ldr, str, jmp, branches
+      wire [15:0] cla_num1 = (JMP || BRANCH) ? i_pc : i_r1data;
+      wire [15:0] cla_num2 = ADD ? i_r2data :
+<<<<<<< HEAD
+                              SUB ? ~i_r2data :
+
+                              ADDIMM ? ({{11{IMM5[4]}}, IMM5}) :
+                              (LDR || STR) ? ({{10{IMM6[5]}}, IMM6}) :
+                              BRANCH ? ({{7{IMM9[8]}}, IMM9}) :
+                              JMP ? ({{5{IMM11[10]}}, IMM11}) :
+                              16'b0; 
+      wire cla_cin = (SUB || BRANCH || JMP);
+      wire [15:0] cla_sum;
+      cla16 c0 (.a(cla_num1), .b(cla_num2), .cin(cla_cin), .sum(cla_sum));
+
+
+
+      //MUL, DIV, MOD
+      //TODO: figure out if r1data and r2data are signed or unsigned
+      // and if the * operator cares
+      wire signed [15:0] mul_op = i_r1data * i_r2data;
+      wire [15:0] div_op, mod_op;
+      lc4_divider d0 (.i_dividend(i_r1data), .i_divisor(i_r2data),
+            .o_remainder(mod_op), .o_quotient(div_op));
+      wire [15:0] muldivmod = MUL ? mul_op :
+                              DIV ? div_op :
+                              MOD ? mod_op :
+                              16'b0;
+
+      
+      //logical operators
+      wire [15:0] and_op = i_r1data & i_r2data;
+      wire [15:0] or_op = i_r1data | i_r2data;
+      wire [15:0] not_op = ~i_r1data;
+      wire [15:0] xor_op = i_r1data ^ i_r2data; 
+      wire [15:0] andimm_op = i_r1data & {{11{IMM5[4]}}, IMM5};
+            //note: i *think* this is how you sign extend...
+      wire [15:0] logicals = AND ? and_op :
+                              OR ? or_op :
+                              NOT ? not_op :
+                              XOR ? xor_op :
+                              ANDIMM ? andimm_op :
+                              16'b0;
+
+
+      //shifts
+      wire [16:0] sll_op = i_r1data << UIMM4;
+      wire [16:0] srl_op = i_r1data >> UIMM4;
+      wire [16:0] sra_op = $signed(i_r1data) >>> UIMM4;
+      wire [15:0] shifts = SLL ? sll_op :
+                        SRL ? srl_op :
+                        SRA ? sra_op :
+                        16'b0;
+
+
+      //comparisons
+      wire [15:0] cmp_num1, cmp_num2;
+<<<<<<< HEAD
+      assign cmp_num1 = (CMPU || CMPIU) ? i_r1data :
+                                          i_r1data;
+      assign cmp_num2 = CMP ? $signed(i_r2data) :
+                        CMPU ? i_r2data :
+                        CMPI ? ({{9{IMM7[6]}}, IMM7}) :
+                        ({9'b0, IMM7}); 
+      wire [15:0] comparisons = (cmp_num1 == cmp_num2) ? 16'b0 :
+            ((CMPU | CMPIU) & (cmp_num1 > cmp_num2)) ? 16'b1 :
+            ((CMPU | CMPIU) & (cmp_num1 < cmp_num2)) ? 16'hFFFF :
+            (cmp_num1[15] & !cmp_num2[15]) ? 16'hFFFF :
+            (!cmp_num1[15] & cmp_num2[15]) ? 16'b1 :
+            (cmp_num1 > cmp_num2) ? 16'b1 : 
+            16'hFFFF;
+=======
+
+
+      //trap, jsr, jsrr      
+      wire [15:0] trapjsrjsrrrtijmpr = TRAP ? (UIMM8 | 16'h8000) :
+                                    JSR ? (16'h8000 & i_pc) | (IMM11 << 4) :
+                                    i_r1data;
+
+
+      //const, hiconst
+      wire [15:0] constants = CONST ? {{7{IMM9[8]}}, IMM9} :
+                              (i_r1data & 16'hFF) | (UIMM8 << 8);
+
+
+
+      //final MUX:
+            //cla_sum = (AND || SUB || ADDIMM || LDR || STR || JMP || BRANCH)
+            //muldivmod = (MUL || DIV || MOD)
+            //logicals = (AND || OR || NOT || XOR || ANDIMM)
+            //comparisons = (CMP || CMPI || CMPU || CMPIU)
+            //shifts = (SLL || SRL || SRA)
+            //trapjsrjsrr = (TRAP || JSR || JSRR)
+            //constants = (CONST || HICONST)
+<<<<<<< HEAD
+      assign o_result = (AND || OR || NOT || XOR || ANDIMM) ? logicals :
+                        (ADD || SUB || ADDIMM || LDR || STR || JMP || BRANCH) ? cla_sum :
+=======
+      assign o_result = (ADD || SUB || ADDIMM || LDR || STR || JMP || BRANCH) ? cla_sum :
+>>>>>>> 2b7ceb8350222c21584ed30ba03abf73d8d4973c
+                        (MUL || DIV || MOD) ? muldivmod :
+                        (CMP || CMPI || CMPU || CMPIU) ? comparisons :
+                        (SLL || SRL || SRA) ? shifts :
+                        (TRAP || JSR || JSRR || RTI || JMPR) ? trapjsrjsrrrtijmpr :
+                        (CONST || HICONST) ? constants :
+                        16'b0;
 
 
       /*** YOUR CODE HERE ***/
--- lab3-singlecycle/lc4_divider.v
+++ lab3-singlecycle/lc4_divider.v
@@ -1,4 +1,5 @@
-/* TODO: name and PennKeys of all group members here */
+/* TODO: INSERT NAME AND PENNKEY HERE */
+//Names: Charlie Gottlieb cbg613, Keith Bonwitt kbonwitt
 
 `timescale 1ns / 1ps
 `default_nettype none
@@ -7,6 +8,116 @@ module lc4_divider(input  wire [15:0] i_dividend,
                    input  wire [15:0] i_divisor,
                    output wire [15:0] o_remainder,
                    output wire [15:0] o_quotient);
+
+      //one_iter 0
+      wire [15:0] dividend_1, remainder_1, quotient_1;
+      lc4_divider_one_iter i0(.i_dividend(i_dividend), .i_divisor(i_divisor),
+             .i_remainder(16'b0), .i_quotient(16'b0),
+             .o_dividend(dividend_1), .o_remainder(remainder_1), .o_quotient(quotient_1));
+
+      //one_iter 1
+      wire [15:0] dividend_2, remainder_2, quotient_2;
+      lc4_divider_one_iter i1(.i_dividend(dividend_1), .i_divisor(i_divisor),
+             .i_remainder(remainder_1), .i_quotient(quotient_1),
+             .o_dividend(dividend_2), .o_remainder(remainder_2), .o_quotient(quotient_2));
+
+      //one_iter 2
+      wire [15:0] dividend_3, remainder_3, quotient_3;
+      lc4_divider_one_iter i2(.i_dividend(dividend_2), .i_divisor(i_divisor),
+             .i_remainder(remainder_2), .i_quotient(quotient_2),
+             .o_dividend(dividend_3), .o_remainder(remainder_3), .o_quotient(quotient_3));
+
+      //one_iter 3
+      wire [15:0] dividend_4, remainder_4, quotient_4;
+      lc4_divider_one_iter i3(.i_dividend(dividend_3), .i_divisor(i_divisor),
+             .i_remainder(remainder_3), .i_quotient(quotient_3),
+             .o_dividend(dividend_4), .o_remainder(remainder_4), .o_quotient(quotient_4));
+
+      //one_iter 4
+      wire [15:0] dividend_5, remainder_5, quotient_5;
+      lc4_divider_one_iter i4(.i_dividend(dividend_4), .i_divisor(i_divisor),
+             .i_remainder(remainder_4), .i_quotient(quotient_4),
+             .o_dividend(dividend_5), .o_remainder(remainder_5), .o_quotient(quotient_5));
+
+      //one_iter 5
+      wire [15:0] dividend_6, remainder_6, quotient_6;
+      lc4_divider_one_iter i5(.i_dividend(dividend_5), .i_divisor(i_divisor),
+             .i_remainder(remainder_5), .i_quotient(quotient_5),
+             .o_dividend(dividend_6), .o_remainder(remainder_6), .o_quotient(quotient_6));
+
+      //one_iter 6
+      wire [15:0] dividend_7, remainder_7, quotient_7;
+      lc4_divider_one_iter i6(.i_dividend(dividend_6), .i_divisor(i_divisor),
+             .i_remainder(remainder_6), .i_quotient(quotient_6),
+             .o_dividend(dividend_7), .o_remainder(remainder_7), .o_quotient(quotient_7));
+
+      //one_iter 7
+      wire [15:0] dividend_8, remainder_8, quotient_8;
+      lc4_divider_one_iter i7(.i_dividend(dividend_7), .i_divisor(i_divisor),
+             .i_remainder(remainder_7), .i_quotient(quotient_7),
+             .o_dividend(dividend_8), .o_remainder(remainder_8), .o_quotient(quotient_8));
+
+      //one_iter 8
+      wire [15:0] dividend_9, remainder_9, quotient_9;
+      lc4_divider_one_iter i8(.i_dividend(dividend_8), .i_divisor(i_divisor),
+             .i_remainder(remainder_8), .i_quotient(quotient_8),
+             .o_dividend(dividend_9), .o_remainder(remainder_9), .o_quotient(quotient_9));
+
+      //one_iter 9
+      wire [15:0] dividend_10, remainder_10, quotient_10;
+      lc4_divider_one_iter i9(.i_dividend(dividend_9), .i_divisor(i_divisor),
+             .i_remainder(remainder_9), .i_quotient(quotient_9),
+             .o_dividend(dividend_10), .o_remainder(remainder_10), .o_quotient(quotient_10));
+
+      //one_iter 10
+      wire [15:0] dividend_11, remainder_11, quotient_11;
+      lc4_divider_one_iter i10(.i_dividend(dividend_10), .i_divisor(i_divisor),
+             .i_remainder(remainder_10), .i_quotient(quotient_10),
+             .o_dividend(dividend_11), .o_remainder(remainder_11), .o_quotient(quotient_11));
+
+      //one_iter 11
+      wire [15:0] dividend_12, remainder_12, quotient_12;
+      lc4_divider_one_iter i11(.i_dividend(dividend_11), .i_divisor(i_divisor),
+             .i_remainder(remainder_11), .i_quotient(quotient_11),
+             .o_dividend(dividend_12), .o_remainder(remainder_12), .o_quotient(quotient_12));
+
+      //one_iter 12
+      wire [15:0] dividend_13, remainder_13, quotient_13;
+      lc4_divider_one_iter i12(.i_dividend(dividend_12), .i_divisor(i_divisor),
+             .i_remainder(remainder_12), .i_quotient(quotient_12),
+             .o_dividend(dividend_13), .o_remainder(remainder_13), .o_quotient(quotient_13));
+
+      //one_iter 13
+      wire [15:0] dividend_14, remainder_14, quotient_14;
+      lc4_divider_one_iter i13(.i_dividend(dividend_13), .i_divisor(i_divisor),
+             .i_remainder(remainder_13), .i_quotient(quotient_13),
+             .o_dividend(dividend_14), .o_remainder(remainder_14), .o_quotient(quotient_14));
+
+      //one_iter 14
+      wire [15:0] dividend_15, remainder_15, quotient_15;
+      lc4_divider_one_iter i14(.i_dividend(dividend_14), .i_divisor(i_divisor),
+             .i_remainder(remainder_14), .i_quotient(quotient_14),
+             .o_dividend(dividend_15), .o_remainder(remainder_15), .o_quotient(quotient_15));
+
+      //one_iter 15
+      wire [15:0] dividend_16, remainder_16, quotient_16;
+      lc4_divider_one_iter i15(.i_dividend(dividend_15), .i_divisor(i_divisor),
+             .i_remainder(remainder_15), .i_quotient(quotient_15),
+             .o_dividend(dividend_16), .o_remainder(remainder_16), .o_quotient(quotient_16));
+
+
+      wire i_divisor_zero;
+      assign i_divisor_zero = i_divisor == 16'b0;
+
+      assign o_remainder = i_divisor_zero ? 16'b0: remainder_16;
+      assign o_quotient = i_divisor_zero ? 16'b0: quotient_16;
+
+
+      //questions for TA
+      //1 do we need to have the [15:0] for assigning wires in the function-things
+      //2 help with git pushing
+      //3 is the iteration right (before we copy paste and stuff)
+      //4 cool to use single-line assignments of complicated stuff?? like remainder_updated
 
       /*** YOUR CODE HERE ***/
 
@@ -19,6 +130,20 @@ module lc4_divider_one_iter(input  wire [15:0] i_dividend,
                             output wire [15:0] o_dividend,
                             output wire [15:0] o_remainder,
                             output wire [15:0] o_quotient);
+
+      assign o_dividend = i_dividend << 1;
+
+      //remainder updater, 16 bits
+      wire [15:0] remainder_updated;
+      assign remainder_updated = (i_dividend >> 15 & 1'b1) | (i_remainder << 1);
+
+      //remainder < divisor, will be used in both muxes, just 1 bit
+      wire remainder_lt_divisor;
+      assign remainder_lt_divisor = remainder_updated < i_divisor;
+
+      assign o_remainder = remainder_lt_divisor ? remainder_updated : (remainder_updated - i_divisor);
+      assign o_quotient = remainder_lt_divisor ? (i_quotient << 1) : ( (i_quotient << 1) | 1'b1);
+
 
       /*** YOUR CODE HERE ***/
 
